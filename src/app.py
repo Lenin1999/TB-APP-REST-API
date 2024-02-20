bf97f130c2dc85b68ba239b25915cadca72f2b36
Lenin1999 <pokibarra1999@gmail.com>
2024-02-20 13:40:11 -0500

    funcionando

--- src/app.py
+++ src/app.py
@@ -1,20 +1,25 @@
 from flask import Flask
 from config import config
+from flask_cors import CORS
 
 #Rutas
 from routes import paciente_controlador
 
-
 app = Flask(__name__)
+
+CORS(app) 
+
+app.register_blueprint(paciente_controlador.main, url_prefix='/api/pacientes')
+
 
 def page_not_found(error):
     return "<h1>Not found page</h1>", 404
 
+app.config.from_object(config['development'])
+
 if __name__ == '__main__':
-    app.config.from_object(config['development'])
+    
+    app.run(host='192.168.1.8', port=5000)
 
     #Blueprints
-    app.register_blueprint(paciente_controlador.main, url_prefix='/api/pacientes')
     app.register_error_handler(404, page_not_found)
-    app.run() 
-
