dc6b0256d3680bd6f17975000825025016ccb5ac
Lenin1999 <pokibarra1999@gmail.com>
2024-02-29 00:18:24 -0500

    ultimo

--- src/models/paciente_modelo.py
+++ src/models/paciente_modelo.py
@@ -22,3 +22,24 @@ class PacienteModel():
         
         except Exception as ex:
             raise Exception(ex)
+
+
+class PacienteModelInsert():
+    @classmethod
+    def post_pacientes(cls, id_medico, nombre, telefono, email, imagen_uri):
+        try:
+            connection = get_connection()
+            with connection.cursor() as cursor:
+                cursor.execute("INSERT INTO pacientes (id_medico, nombre, telefono, email, imagen_uri) VALUES (%s, %s, %s, %s, %s)",
+                               (id_medico, nombre, telefono, email, imagen_uri))
+            connection.commit()  # Es importante hacer commit después de una operación de escritura en la base de datos
+
+            # Puedes retornar algo si lo necesitas, por ejemplo, el ID del paciente insertado
+            # En este caso, el ID es un valor serial, por lo que puedes obtenerlo después del INSERT
+            # Ejemplo: paciente_id = cursor.lastrowid
+
+            connection.close()
+            return "Paciente insertado exitosamente"
+        
+        except Exception as ex:
+            raise Exception(ex)
--- src/routes/paciente_controlador.py
+++ src/routes/paciente_controlador.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, jsonify
+from flask import Flask, request, jsonify
 
 #modelos
 from models.paciente_modelo import PacienteModel
+from models.paciente_modelo import PacienteModelInsert
 
 main=Blueprint('paciente_blueprint', __name__)
 
@@ -12,3 +14,31 @@ def get_pacientes():
         return jsonify(pacientes)
     except Exception as ex:
         return jsonify({'messaage': str(ex)}), 500
+    
+
+
+@main.route('/crear_paciente', methods=['POST'])
+def crear_paciente():
+    try:
+        # Obtener los datos del paciente del cuerpo de la solicitud
+        id_medico = request.form.get('id_medico')
+        nombre = request.form.get('nombre')
+        telefono = request.form.get('telefono')
+        email = request.form.get('email')
+        imagen_uri = request.form.get('imagen_uri')
+
+        print(id_medico);
+        print(nombre);
+        print(telefono);
+        print(email);
+        
+
+        # Insertar el paciente en la base de datos
+        resultado = PacienteModelInsert.post_pacientes(id_medico, nombre, telefono, email, imagen_uri)
+        print(resultado);
+        # Retornar una respuesta
+        return jsonify({"mensaje": resultado}), 201  # 201 significa "Created"
+    
+    except Exception as ex:
+        # Manejar errores
+        return jsonify({"error": str(ex)}), 500
